d4df63edf58d34eeb4fbf62197b8380255b2d57d
Adson Pereira <adsongarapinha@gmail.com>
2025-07-05 03:17:04 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,16 @@
-import axios from "axios"
- 
+import axios from "axios";
+
 const api = axios.create({
-    //baseURL: 'https://satoshinakamoto.run/api',
-    baseURL: 'http://localhost:8080',
-})
+  baseURL: "http://localhost:8080", // ou sua URL em produção
+});
+
+// Interceptor que adiciona o token automaticamente
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
