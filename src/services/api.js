ad1ab0e120fecc4145380281b94575da3061c471
Adson Pereira <adsongarapinha@gmail.com>
2025-07-06 01:58:54 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // ou sua URL em produção
+  baseURL: "http://localhost:8081", // ou sua URL em produção
 });
 
 // Interceptor que adiciona o token automaticamente
