89ce7cfb333fc6d001fbbb36cea960dad1533b37
Adson Pereira <adsongarapinha@gmail.com>
2025-07-09 02:57:38 -0300

    Update gateways.routes.js

--- api/src/routes/gateways.routes.js
+++ api/src/routes/gateways.routes.js
@@ -1,15 +1,10 @@
 const { authenticateToken } = require("../functions/index.js");
-
 module.exports = app => {
   const gateways = require("../controllers/gateways.controller.js");
-
   var router = require("express").Router();
-
   router.post("/gateway", authenticateToken, gateways.gateway);
-
   router.get("/allgateways", authenticateToken, gateways.gateways);
   router.post('/addgateway', authenticateToken, gateways.createGateway);
   router.delete("/gateway/:id", authenticateToken, gateways.deleteGateway);
-
   app.use("/api/gateways", router);
 };
