c1d27fbb3a9490974a1c65bc7d24464f43e4a6dd
Adson Pereira <adsongarapinha@gmail.com>
2025-07-09 02:57:47 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,25 @@ import "react-toastify/dist/ReactToastify.css";
 import Routes from "./routes";
 import { UserProvider } from "@src/context/userContext";
 import { GlobalStyle } from "@src/styles/globalStyles";
-
 function App() {
   return (
     <>
       <UserProvider>
         <Routes />
       </UserProvider>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <GlobalStyle />
     </>
   );
 }
-
 export default App;
